bb5a03ecc24a858663a05da095c764b4fae7a2f8
Alex Miller <apmiller108@yahoo.com>
2015-12-13 09:43:50 -0500

    Add FeedViewController Class

---
+++ Single View/FeedViewController.h
@@ -0,0 +1,13 @@
+//
+//  FeedViewController.h
+//  Single View
+//
+//  Created by alex miller on 12/13/15.
+//  Copyright © 2015 alex miller. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface FeedViewController : NSViewController
+
+@end
---
+++ Single View/FeedViewController.m
@@ -0,0 +1,22 @@
+//
+//  FeedViewController.m
+//  Single View
+//
+//  Created by alex miller on 12/13/15.
+//  Copyright © 2015 alex miller. All rights reserved.
+//
+
+#import "FeedViewController.h"
+
+@interface FeedViewController ()
+
+@end
+
+@implementation FeedViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
