fb1bc6118a4355bb1352f875fc1bd7c9f0c86f4e
Alex Miller <apmiller108@yahoo.com>
2015-12-14 09:19:57 -0500

    Add profile to navigation controller.

--- Single View/FeedViewController.m
+++ Single View/FeedViewController.m
@@ -8,6 +8,7 @@
 
 #import "FeedViewController.h"
 #import "FavoritesViewController.h"
+#import "ProfileViewController.h"
 
 @interface FeedViewController ()
 
@@ -25,17 +26,31 @@
     self.view.backgroundColor = [UIColor blueColor];
     // set the controller tab icon
     self.tabBarItem.image = [UIImage imageNamed:@"Stack of Photos-24"];
+    
     // Favorites Button
     UIButton *favoritesButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     favoritesButton.frame = CGRectMake(60, 100, 200, 44);
     [favoritesButton setTitle:@"View Favorites" forState:UIControlStateNormal];
     [self.view addSubview:favoritesButton];
     [favoritesButton addTarget:self action:@selector(showFavorites) forControlEvents:UIControlEventTouchUpInside];
+    
+    // Profile Button
+    UIButton *profileButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    profileButton.frame = CGRectMake(60, 120, 200, 44);
+    [profileButton setTitle:@"View Profile" forState:UIControlStateNormal];
+    [self.view addSubview:profileButton];
+    [profileButton addTarget:self action:@selector(showProfile) forControlEvents:UIControlEventTouchUpInside];
+    
 }
 
 - (void)showFavorites {
     FavoritesViewController *favoritesViewController = [[FavoritesViewController alloc] init];
     [self.navigationController pushViewController:favoritesViewController animated:YES];
+}
+
+- (void)showProfile {
+    ProfileViewController *profileViewController = [[ProfileViewController alloc] init];
+    [self.navigationController pushViewController:profileViewController animated:YES];
 }
 
 - (void)didReceiveMemoryWarning {
